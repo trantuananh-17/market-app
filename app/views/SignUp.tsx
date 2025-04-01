af7f2bce830fb27381d72011432d8d313a57decf
TunAnhhaui <anhkyohauik17@gmail.com>
2025-04-01 16:02:12 +0700

    validate form

--- app/views/SignUp.tsx
+++ app/views/SignUp.tsx
@@ -1,23 +1,70 @@
 import FormInput from "@ui/FormInput";
 import WelcomeHeader from "@ui/WelcomeHeader";
-import { FC } from "react";
-import {
-  View,
-  StyleSheet,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-} from "react-native";
+import { FC, useState } from "react";
+import { View, StyleSheet } from "react-native";
 import AppButton from "@ui/AppButton";
 import FormDivider from "@ui/FormDivider";
 import FormNavigator from "@ui/FormNavigator";
 import CustomKeyboardAvoidingView from "@ui/CustomKeyboardAvoidingView";
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { AuthStackParamList } from "app/navigator/Auth";
+import * as yup from "yup";
+import axios from "axios";
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex =
+  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]+$/;
+
+yup.addMethod(yup.string, "email", function validateEmail(message) {
+  return this.matches(emailRegex, {
+    message,
+    name: "email",
+    excludeEmptyString: true,
+  });
+});
+
+export const newUserSchema = yup.object({
+  name: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .required("password is required")
+    .min(6, "Password must be at least 6 characters long.")
+    .matches(
+      passwordRegex,
+      "Password must include: A-Z, a-z, 0-9, and special character (!@#$%^&*)."
+    ),
+});
 
 interface Props {}
 
 const SignUp: FC<Props> = (props) => {
+  const [userInfo, setUserInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (name: string) => {
+    return (text: string) => {
+      setUserInfo({ ...userInfo, [name]: text });
+    };
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const info = await newUserSchema.validate(userInfo);
+      axios.post("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { email, name, password } = userInfo;
+
   const { navigate } = useNavigation<NavigationProp<AuthStackParamList>>();
 
   return (
@@ -29,15 +76,26 @@ const SignUp: FC<Props> = (props) => {
           subHeading="Create your account to buy, sell, and chat directly with real people around you."
         />
         <View style={styles.formContainer}>
-          <FormInput placeholder="Name..." />
+          <FormInput
+            placeholder="Name..."
+            value={name}
+            onChangeText={handleChange("name")}
+          />
           <FormInput
             placeholder="Email..."
             keyboardType="email-address"
             autoCapitalize="none"
+            value={email}
+            onChangeText={handleChange("email")}
           />
-          <FormInput placeholder="Password..." secureTextEntry />
+          <FormInput
+            placeholder="Password..."
+            secureTextEntry
+            value={password}
+            onChangeText={handleChange("password")}
+          />
 
-          <AppButton title="Sign Up" />
+          <AppButton title="Sign Up" onPress={handleSubmit} />
 
           <FormDivider />
 
